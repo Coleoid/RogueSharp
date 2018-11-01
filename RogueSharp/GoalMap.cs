8002cb275c996a17c0a27a62e609a4e28f7719c5
Jason Cole <coleoid@yahoo.com>
2018-11-01 09:18:56 -0400

    Save final file in Coord -> Point move

--- RogueSharp/GoalMap.cs
+++ RogueSharp/GoalMap.cs
@@ -548,44 +548,6 @@ namespace RogueSharp
          return mapRepresentation.ToString().TrimEnd( '\r', '\n' );
       }
 
-      private struct WeightedPoint : IEquatable<WeightedPoint>
-      {
-         private Point _point;
-         public int Weight
-         {
-            get; set;
-         }
-
-         public int X
-         {
-            get
-            {
-               return _point.X;
-            }
-            set
-            {
-               //_point.X = value;
-            }
-         }
-
-         public int Y
-         {
-            get
-            {
-               return _point.Y;
-            }
-            set
-            {
-               //_point.Y = value;
-            }
-         }
-
-         public bool Equals( WeightedPoint other )
-         {
-            return X == other.X && Y == other.Y && Weight == other.Weight;
-         }
-      }
-
       private class GoalMapPathFinder
       {
          private readonly GoalMap _goalMap;
